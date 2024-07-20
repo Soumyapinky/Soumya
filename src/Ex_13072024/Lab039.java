5f499cb817b58db3f55f37a6e72bfcb357d4687d
soumyaYendra <yendra.soumya1221@gmail.com>
2024-07-20 21:50:35 +0530

    new file

---
+++ src/Ex_13072024/Lab039.java
@@ -0,0 +1,13 @@
+package Ex_13072024;
+
+public class Lab39 {
+    public static void main(String[] args) {
+        String firstname = "PRAMOD";
+        String lastname = "DUTTA";
+        System.out.println(firstname + lastname);
+
+        byte a = 10;
+        byte b = 18;
+        System.out.println(a+b);
+    }
+}
